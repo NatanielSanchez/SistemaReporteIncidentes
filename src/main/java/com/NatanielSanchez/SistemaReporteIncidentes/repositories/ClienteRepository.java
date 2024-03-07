7b311a0da36bed509a0c496eef36f83b55034265
NatanielSanchez <natanielsanchez2077@gmail.com>
2024-03-07 16:35:33 -0300

    -Implementada busqueda filtrada de Clientes por nombre, identificacion y/o tipo de cliente.

--- src/main/java/com/NatanielSanchez/SistemaReporteIncidentes/repositories/ClienteRepository.java
+++ src/main/java/com/NatanielSanchez/SistemaReporteIncidentes/repositories/ClienteRepository.java
@@ -1,11 +1,42 @@
 package com.NatanielSanchez.SistemaReporteIncidentes.repositories;
 
 import com.NatanielSanchez.SistemaReporteIncidentes.models.Cliente;
+import com.NatanielSanchez.SistemaReporteIncidentes.models.TipoCliente;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 
-public interface ClienteRepository extends JpaRepository<Cliente, Long>
+public interface ClienteRepository extends JpaRepository<Cliente, Long>, JpaSpecificationExecutor<Cliente>
 {
     Optional<Cliente> findByIdentificacion(String identificacion);
+
+    interface Specs
+    {
+        static Specification<Cliente> nombreLike(String nombre)
+        {
+            return (clienteRoot, cq, cb) ->
+                    cb.like(clienteRoot.get("nombre"), "%" + nombre.toUpperCase() + "%");
+        }
+
+        static Specification<Cliente> identificacionLike(String identificacion)
+        {
+            return (clienteRoot, cq, cb) ->
+                    cb.like(clienteRoot.get("identificacion"), "%" + identificacion.toUpperCase() + "%");
+        }
+
+        static Specification<Cliente> tipoClienteLike(String tipo)
+        {
+            return (clienteRoot, cq, cb) -> {
+                Join<Cliente, TipoCliente> clienteTipoClienteJoin = clienteRoot.join("tipoCliente", JoinType.INNER);
+                return cb.like(clienteTipoClienteJoin.get("tipo"), "%" + tipo.toUpperCase() + "%");
+            };
+
+        }
+
+
+    }
 }
